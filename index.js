213830831f615889908342a32760f70b2594bb1c
Jim Kang <jimkang@gmail.com>
2016-01-07 23:42:50 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,37 @@
+var director = require('director');
+
+function initPlayerRouter(opts) {
+  var seekParamName;
+  var seekResponder;
+
+  if (opts) {
+    if (opts.seeking) {
+      seekParamName = opts.seeking.seekParamName;
+      seekResponder = opts.seeking.seekResponder;
+    }
+  }
+
+  window.onhashchange = route;
+
+  function route() {
+    var segments = window.location.hash.slice(1).split('/');
+    if (segments.length > 0) {
+      var lastSegment = segments[segments.length - 1];
+      var paramAndValue = lastSegment.split('=');
+      if (paramAndValue.length === 2) {
+        if (paramAndValue[0] === seekParamName) {
+          var value = parseInt(paramAndValue[1], 10);
+          if (isNaN(value)) {
+            value = 0;
+          }
+          seekResponder(value);
+        }
+      }
+    }
+
+    // If more traditional routing was needed, this is where we would pass the
+    // hash off to Director or another routing module.
+  }  
+}
+
+module.exports = initPlayerRouter;
---
+++ tests/basictests.js
@@ -0,0 +1,95 @@
+var test = require('tape');
+var initPlayerRouter = require('../index');
+
+// test('Pause', function pause(t) {
+//   console.log('Enter `c()` in the console to continue.');
+//   window.c = t.end;
+// });
+
+var positiveCases = [
+  {
+    seekParamName: 'start',
+    hashLocation: 'start=10',
+    expected: 10
+  },
+  {
+    seekParamName: 's',
+    hashLocation: 's=10',
+    expected: 10
+  },
+  {
+    seekParamName: 's',
+    hashLocation: 'whatever/something/s=20',
+    expected: 20
+  },
+  {
+    seekParamName: 'start',
+    hashLocation: 'start=abcd20',
+    expected: 0
+  },
+  {
+    seekParamName: 'init',
+    hashLocation: 'next=hello/init=801',
+    expected: 801
+  }
+];
+
+var negativeCases = [
+  {
+    seekParamName: 'start',
+    hashLocation: ''
+  },
+  {
+    seekParamName: 'start',
+    hashLocation: 'nothingtodowithseek=50'
+  },
+  {
+    seekParamName: 's',
+    hashLocation: 's=10/something-after'
+  }
+];
+
+positiveCases.forEach(runPositiveCaseTest);
+negativeCases.forEach(runNegativeCaseTest);
+
+function runPositiveCaseTest(testCase) {
+  test('Positive test', function positiveTest(t) {
+    initPlayerRouter({
+      seeking: {
+        seekParamName: testCase.seekParamName,
+        seekResponder: checkRouting
+      }
+    });
+
+    window.location.hash = testCase.hashLocation;
+
+    function checkRouting(time) {
+      t.equal(time, testCase.expected, 'Correct time is passed to responder.');
+      t.end();
+    }
+  });
+}
+
+function runNegativeCaseTest(testCase) {
+  test('Negative test', function negativeTest(t) {
+    initPlayerRouter({
+      seeking: {
+        seekParamName: testCase.seekParamName,
+        seekResponder: checkRouting
+      }
+    });
+
+    window.location.hash = testCase.hashLocation;
+
+    setTimeout(pass, 0);
+
+    function checkRouting() {
+      t.fail('Does not call responder because hash does not contain param.');
+    }
+
+    function pass() {
+      t.pass('Does not call responder because hash does not contain param.');
+      t.end();
+    }
+  });
+}
